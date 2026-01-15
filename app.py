5dc116ccec1f572aa9a27fbac9c8f98148418610
Orson-M <orson.meilland@gmail.com>
2026-01-15 13:06:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import pyxel
 pyxel.init(320, 184, title="JeuNSI")
-player_x = 0
+player_x = 30
 player_y = 0
 wallside = False
 grounded = True 
 climbing = False
 vy = 0
 # pget(x, y) to get the status
-def player_deplacement(x,y):
+def player_deplacement(x):
     """déplacement avec les touches de directions"""
 
     if pyxel.btn(pyxel.KEY_D) == True:
@@ -25,7 +25,7 @@ def player_deplacement(x,y):
     #if pyxel.btn(pyxel.KEY_SPACE):
        # if (player_y < 320-16):
             #player_y = player_y + 8
-    return x,y
+    return x
 
 def climb(wallside, climbing, player_y):
     if wallside == True and pyxel.btn(pyxel.KEY_UP) == True:
@@ -48,19 +48,19 @@ def touch_ground(player_x, player_y, grounded):
 
 def update():
     global x,y,player_x,player_y,vy
-    if pyxel.btnp(pyxel.KEY_SPACE) and pyxel.pget(player_x, player_y - 1) == 0:
+    if pyxel.btnp(pyxel.KEY_Z) :
         vy = -5
     vy += 1
     player_y += vy
-    if player_y > 184:
+    if player_y > 101:
         vy = 0
-        player_y=184
+        player_y=101
 
-    player_x, player_y = player_deplacement(player_x, player_y)
+    player_x = player_deplacement(player_x)
     
 def draw():
     pyxel.cls(1)
-    pyxel.rect(player_x,player_y,4,8,2)
+    pyxel.rect(player_x,player_y,6,12,2)
 
 
 pyxel.run(update,draw)
