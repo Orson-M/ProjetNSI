394f2c55be4d48e6b98c7232af685a2b91063cde
Orson-M <orson.meilland@gmail.com>
2026-01-29 18:45:07 +0100

    Climbing test

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ pyxel.init(320, 184, title="JeuNSI")
 pyxel.load("my_resource.pyxres")
 x = 20
 y = 0
+alt_init = y
 wallside = False
 climbing = False
 vy = 0
@@ -10,7 +11,7 @@ vy = 0
 # pget(x, y) to get the status
 
 def update():
-    global x,y,vy
+    global x,y,vy,alt_init,wallside,climbing
     print(x,y)
 
     if pyxel.btn(pyxel.KEY_D) == True and pyxel.pget(x+7, y+10) == 0:
@@ -23,14 +24,29 @@ def update():
         vy += 0.5
     if pyxel.pget(x,y+13) != 0:
         vy = 0
-    if pyxel.btnp(pyxel.KEY_SPACE) and pyxel.pget(x,y+13) != 0:
-        y = y - 12
-    
+
+    if pyxel.btnp(pyxel.KEY_SPACE) or pyxel.btnp(pyxel.KEY_SPACE) and pyxel.pget(x,y+13) != 0:
+        alt_init = y
+        if y - alt_init < 20 and pyxel.pget(x,y+13) != 0:
+            vy = -5
+        if y - alt_init > 20 or pyxel.pget(x,y-1) != 0:
+            vy = 0
     if y > 320:
         vy = 0
         x = 0 
         y = 20
     
+    """if pyxel.btnp(pyxel.KEY_E or pyxel.KEY_A) and pyxel.pget(x+7,y-12 or x-1,y-12) != 0:
+        climbing = True
+    else:
+        climbing = False
+
+    if climbing == True:
+        y -= 2
+    if climbing == False:
+        y = 0"""
+    
+
     y += vy
 
     
