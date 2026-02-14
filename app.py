ed703e92a3f4c0ee8736e179bd558e163319073f
Orson-M <orson.meilland@gmail.com>
2026-02-14 20:04:27 +0100

    idk but it dont work

--- app.py
+++ app.py
@@ -7,8 +7,166 @@ alt_init = y
 wallside = False
 climbing = False
 vy = 0
+rhitbox = False
+lhitbox = False
+uhitbox = False
+dhitbox = False
 
 # pget(x, y) to get the status
+
+def hitbox (x,y):
+    global rhitbox, lhitbox, uhitbox, dhitbox
+    # up 1
+    if pyxel.pget(x,y-1) != 0:
+        uhitbox = True
+        return uhitbox
+    # up 2
+    elif pyxel.pget(x+1,y-1) != 0:
+        uhitbox = True
+        return uhitbox
+    # up 3
+    elif pyxel.pget(x+2,y-1) != 0:
+        uhitbox = True
+        return uhitbox 
+    # up 4
+    elif pyxel.pget(x+3,y-1) != 0:
+        uhitbox = True
+        return uhitbox    
+    # up 5
+    elif pyxel.pget(x+4,y-1) != 0:
+        uhitbox = True
+        return uhitbox
+    # up 6
+    elif pyxel.pget(x+5,y-1) != 0:
+        uhitbox = True
+        return uhitbox
+    else:
+         uhitbox = False
+         return uhitbox
+
+    # dn 1
+    if pyxel.pget(x,y+13) != 0:
+        dhitbox = True
+        return dhitbox
+    # dn 2
+    if pyxel.pget(x+1,y+13) != 0:
+        dhitbox = True
+        return dhitbox
+    # dn 3
+    if pyxel.pget(x+2,y+13) != 0:
+        dhitbox = True
+        return dhitbox
+    # dn 4
+    if pyxel.pget(x+3,y+13) != 0:
+        dhitbox = True
+        return dhitbox
+    # dn 5
+    if pyxel.pget(x+4,y+13) != 0:
+        dhitbox = True
+        return dhitbox
+    # dn 6
+    if pyxel.pget(x+5,y+13) != 0:
+        dhitbox = True
+        return dhitbox
+
+    #left 1
+    if pyxel.pget(x-1,y) != 0:
+        lhitbox = True
+        return lhitbox
+    #left 2
+    if pyxel.pget(x-1,y+1) != 0:
+        lhitbox = True
+        return lhitbox
+    #left 3
+    if pyxel.pget(x-1,y+2) != 0:
+        lhitbox = True
+        return lhitbox
+    #left 4
+    if pyxel.pget(x-1,y+3) != 0:
+        lhitbox = True
+        return lhitbox
+    #left 5
+    if pyxel.pget(x-1,y+4) != 0:
+        lhitbox = True
+        return lhitbox
+    #left 6
+    if pyxel.pget(x-1,y+5) != 0:
+        lhitbox = True
+        return lhitbox
+    #left 7
+    if pyxel.pget(x-1,y+6) != 0:
+        lhitbox = True
+        return lhitbox
+    #left 8
+    if pyxel.pget(x-1,y+7) != 0:
+        lhitbox = True
+        return lhitbox
+    #left 9
+    if pyxel.pget(x-1,y+8) != 0:
+        lhitbox = True
+        return lhitbox
+    #left 10
+    if pyxel.pget(x-1,y+9) != 0:
+        lhitbox = True
+        return lhitbox
+    #left 11
+    if pyxel.pget(x-1,y+10) != 0:
+        lhitbox = True
+        return lhitbox
+    #left 12
+    if pyxel.pget(x-1,y+11) != 0:
+        lhitbox = True
+        return lhitbox
+
+    #right 1
+    if pyxel.pget(x+7,y) != 0:
+        rhitbox = True
+        return rhitbox
+    #right 2
+    if pyxel.pget(x+7,y+1) != 0:
+        rhitbox = True
+        return rhitbox
+    #right 3
+    if pyxel.pget(x+7,y+2) != 0:
+        rhitbox = True
+        return rhitbox
+    #right 4
+    if pyxel.pget(x+7,y+3) != 0:
+        rhitbox = True
+        return rhitbox
+    #right 5
+    if pyxel.pget(x+7,y+4) != 0:
+        rhitbox = True
+        return rhitbox
+    #right 6    
+    if pyxel.pget(x+7,y+5) != 0:
+        rhitbox = True
+        return rhitbox
+    #right 7
+    if pyxel.pget(x+7,y+6) != 0:
+        rhitbox = True
+        return rhitbox
+    #right 8
+    if pyxel.pget(x+7,y+7) != 0:
+        rhitbox = True
+        return rhitbox
+    #right 9
+    if pyxel.pget(x+7,y+8) != 0:
+        rhitbox = True
+        return rhitbox
+    #right 10
+    if pyxel.pget(x+7,y+9) != 0:
+        rhitbox = True
+        return rhitbox
+    #right 11
+    if pyxel.pget(x+7,y+10) != 0:
+        rhitbox = True
+        return rhitbox
+    #right 12
+    if pyxel.pget(x+7,y+11) != 0:
+        rhitbox = True
+        return rhitbox
+
 
 def update():
     global x,y,vy,alt_init,wallside,climbing
